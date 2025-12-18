5a00e3b46bc35b2abfcc5788c8496a214403772a
Bash Elliott <rackodo.business@gmail.com>
2025-12-19 00:55:23 +1100

    Add prettier config, implement reading of resource packs as objects
    Test reading from the resource pack by logging the description of each pack's mcmeta file. It works!

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
-const packs = [
-	"ddm",
-	"faithful",
-	"immersive"
-]
+const fs = require("fs");
+const path = require("path");
 
-const fs = require('fs/promises');
+const packPaths = ["ddm", "faithful", "immersive"];
+const packs = [];
 
+/**
+ * Recursive function for turning a directory into an Object.
+ * @param {string} dirPath path of the directory
+ * @returns {object} folder represented as an object
+ */
+function dirToObj(dirPath) {
+	const result = {};
+
+	const items = fs.readdirSync(dirPath, { withFileTypes: true });
+
+	items.forEach((item) => {
+		const fullPath = path.join(dirPath, item.name);
+
+		if (item.isDirectory()) {
+			result[item.name] = dirToObj(fullPath);
+		} else {
+			result[item.name] = `${item.parentPath}/${item.name}`;
+		}
+	});
+
+	return result;
+}
+
+/**
+ * Converts pack paths into pack objects.
+ */
+packPaths.forEach((pp) => {
+	packs.push(dirToObj(`packs/${pp}`));
+	// console.log(dirToObj(`packs/${pp}`).assets.minecraft.textures.gui);
+});
+
+/**
+ * Loops over each pack, reads the mcmeta file, spits out the description. This works!
+ */
 packs.forEach((pack) => {
-	fs.readdir(`packs/${pack}/assets/minecraft/textures/gui`, { recursive: false, withFileTypes: true })
-	.then((files) => {
-		files.forEach((file) => {
-			console.log(pack, file.isFile() ? "File" : "Folder", file.name)
-		})
-	})
-	.catch((err) => {throw err})
-})
+	fs.readFile(pack["pack.mcmeta"], "utf-8", (err, data) => {
+		if (err) throw err;
+		const meta = JSON.parse(data);
+		console.log(meta["pack"]["description"]);
+		console.log("");
+	});
+});
